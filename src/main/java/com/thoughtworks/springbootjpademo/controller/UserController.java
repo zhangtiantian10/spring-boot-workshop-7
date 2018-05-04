49f733cf4d4ed63e504f36e28a56080d532946af
NatasG <gaoshuai1205@gmail.com>
2018-05-04 23:30:12 +0800

    add controller and web page for user list

---
+++ src/main/java/com/thoughtworks/springbootjpademo/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.springbootjpademo.controller;
+
+import com.thoughtworks.springbootjpademo.entity.User;
+import com.thoughtworks.springbootjpademo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping()
+    public String list(Model model) {
+        List<User> userList = userRepository.findAll();
+        model.addAttribute("userList", userList);
+        return "user/list";
+    }
+
+
+}
