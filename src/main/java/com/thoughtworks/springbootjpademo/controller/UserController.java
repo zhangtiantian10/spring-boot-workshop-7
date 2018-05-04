77876c1bf5475ab25f263bc667a3685a78644099
NatasG <gaoshuai1205@gmail.com>
2018-05-05 00:28:37 +0800

    add pagination for user list

--- src/main/java/com/thoughtworks/springbootjpademo/controller/UserController.java
+++ src/main/java/com/thoughtworks/springbootjpademo/controller/UserController.java
@@ -3,15 +3,13 @@ package com.thoughtworks.springbootjpademo.controller;
 import com.thoughtworks.springbootjpademo.entity.User;
 import com.thoughtworks.springbootjpademo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("users")
@@ -21,8 +19,10 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping()
-    public String list(Model model) {
-        List<User> userList = userRepository.findAll(Sort.by("id").ascending());
+    public String list(@RequestParam(name = "page", defaultValue = "0") Integer page,
+                       @RequestParam(name = "size", defaultValue = "10") Integer size, Model model) {
+        Pageable pageable = PageRequest.of(page, size, Sort.Direction.ASC, "id");
+        Page<User> userList = userRepository.findAll(pageable);
         model.addAttribute("userList", userList);
         return "user/list";
     }
